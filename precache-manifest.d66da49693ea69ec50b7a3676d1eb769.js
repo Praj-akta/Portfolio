3dc41d217ca0da096c0dbdaccf52db2a30ff6202
Prajakta Limje <prajaktalimje@Prajaktas-Air.Dlink>
2021-01-15 13:17:46 +0530

    Updates

---
+++ precache-manifest.d66da49693ea69ec50b7a3676d1eb769.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c6c3781caf4e8ab6c9c322eb1be651",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "24bd91ddf77ff443fbff",
+    "url": "/portfolio/static/css/main.d78ec236.chunk.css"
+  },
+  {
+    "revision": "fc847b163aadfc95c91d",
+    "url": "/portfolio/static/js/2.b00415bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b00415bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24bd91ddf77ff443fbff",
+    "url": "/portfolio/static/js/main.5690abcf.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c9d072a2887263571900415052c7e678",
+    "url": "/portfolio/static/media/angular.c9d072a2.png"
+  },
+  {
+    "revision": "edb9c6ada1c465e25689dfe0fd811aa8",
+    "url": "/portfolio/static/media/arrow-up.edb9c6ad.svg"
+  },
+  {
+    "revision": "5f928e1ce78dde13f6aa5682c11ffedd",
+    "url": "/portfolio/static/media/background-work.5f928e1c.jpg"
+  },
+  {
+    "revision": "22a7a15a52a0b154cd5d33573d99eef1",
+    "url": "/portfolio/static/media/bootstrap.22a7a15a.png"
+  },
+  {
+    "revision": "46eba83ce45dea7a4609b6e385a79aa1",
+    "url": "/portfolio/static/media/css.46eba83c.png"
+  },
+  {
+    "revision": "bd4fc69dc9ab3de8ee500f2f0653bcf8",
+    "url": "/portfolio/static/media/html.bd4fc69d.png"
+  },
+  {
+    "revision": "3e8aa037ab939018b045bb266b9ce6c7",
+    "url": "/portfolio/static/media/js.3e8aa037.png"
+  },
+  {
+    "revision": "1714929a97119ca71a0652d464b8a200",
+    "url": "/portfolio/static/media/linkedin.1714929a.png"
+  },
+  {
+    "revision": "8753f4f8430b1d9cecfd5fec4c9170f1",
+    "url": "/portfolio/static/media/phone-wallpaper.8753f4f8.png"
+  },
+  {
+    "revision": "f5b54625493f9788358639c7d5378aa9",
+    "url": "/portfolio/static/media/profile-photo.f5b54625.jpg"
+  },
+  {
+    "revision": "52c69740ccf62118fe6676256633f511",
+    "url": "/portfolio/static/media/react-native.52c69740.png"
+  },
+  {
+    "revision": "65fe5ca450758865a59d82b01d637a6e",
+    "url": "/portfolio/static/media/react.65fe5ca4.png"
+  },
+  {
+    "revision": "9e3bc8e0a17798475272eff12f493de5",
+    "url": "/portfolio/static/media/resume.9e3bc8e0.pdf"
+  },
+  {
+    "revision": "6597cc35589236a430e20f4d2adf09a6",
+    "url": "/portfolio/static/media/sass.6597cc35.png"
+  },
+  {
+    "revision": "05b62d0bde85c26927dba9f0429ab904",
+    "url": "/portfolio/static/media/wifi.05b62d0b.png"
+  }
+]);
